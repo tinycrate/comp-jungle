037fef339da3a2aba8b19145807c5d87f63c1375
Ronald Chan <ronaldchanchl0@gmail.com>
2018-10-31 11:04:49 +0800

    Updated project structure to somewhat comply with the MVC requirements
    Feel free to suggest another way of organizing the project if you find a better one!

---
+++ src/hk/edu/polyu/comp/comp2021/jungle/common/controllers/ApplicationController.java
@@ -0,0 +1,10 @@
+package hk.edu.polyu.comp.comp2021.jungle.common.controllers;
+
+import hk.edu.polyu.comp.comp2021.jungle.common.models.JungleGame;
+
+public class ApplicationController {
+    public void Run(){
+        JungleGame game = new JungleGame();
+        // starts the game
+    }
+}
