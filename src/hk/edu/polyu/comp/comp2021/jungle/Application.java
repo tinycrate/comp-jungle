e0f5c22b159b5ec0a779dd46a4d61de95ec9d8b6
Ronald Chan <12611939+tinycrate@users.noreply.github.com>
2018-11-12 22:02:53 +0800

    Updated example javadoc We MUST follow this because this is getting graded for so many marks fml

--- src/hk/edu/polyu/comp/comp2021/jungle/Application.java
+++ src/hk/edu/polyu/comp/comp2021/jungle/Application.java
@@ -2,9 +2,16 @@ package hk.edu.polyu.comp.comp2021.jungle;
 
 import hk.edu.polyu.comp.comp2021.jungle.controllers.ApplicationController;
 
+/**
+ * The main class for the Jungle Game
+ */
 public class Application {
 
-    public static void main(String[] args){
+    /**
+     * The entry point of the application
+     * @param args Command line parameters
+     */
+    public static void main(String[] args) {
         ApplicationController app = new ApplicationController();
         app.Run();
     }
