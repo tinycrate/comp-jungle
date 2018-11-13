8d80c1e5cb489b1ec8d6e7e4dbf26d8cc62ac774
Ronald Chan <12611939+tinycrate@users.noreply.github.com>
2018-11-13 22:47:32 +0800

    Declared and partially implemented the basic structure of the project

---
+++ src/hk/edu/polyu/comp/comp2021/jungle/models/Coordinates.java
@@ -0,0 +1,64 @@
+package hk.edu.polyu.comp.comp2021.jungle.models;
+
+/**
+ * A simple immutable class for keeping board coordinates
+ */
+public class Coordinates {
+
+    /**
+     * Checks whether a string represents valid coordinates
+     *
+     * @param coords The coordinates in String format: "A1" ~ "G9"
+     * @return True if the coordinates are valid
+     */
+    public static boolean isValid(String coords) {
+        if (coords.length() != 2) return false;
+        if (coords.charAt(0) < 'A' || coords.charAt(0) > 'G') return false;
+        return coords.charAt(1) >= '1' && coords.charAt(1) <= '9';
+    }
+
+    private final int x;
+    private final int y;
+
+    /**
+     * Construct the class with integral values
+     *
+     * @param x The x coordinates from the board
+     * @param y The y coordinates from the board
+     */
+    public Coordinates(int x, int y) {
+        if (x < 0 || x >= Board.BOARD_WIDTH) throw new IllegalArgumentException("Invalid x coordinate specified");
+        if (y < 0 || y >= Board.BOARD_HEIGHT) throw new IllegalArgumentException("Invalid y coordinate specified");
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Construct the class with a string value
+     *
+     * @param coords The coordinates in String format: "A1" ~ "G9"
+     */
+    public Coordinates(String coords) {
+        coords = coords.toUpperCase();
+        if (isValid(coords)) {
+            this.x = coords.charAt(0) - 'A';
+            this.y = coords.charAt(1) - '1';
+        } else {
+            throw new IllegalArgumentException("Invalid string coordinates specified");
+        }
+    }
+
+    /**
+     * @return Returns the x coordinate
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * @return Returns the y coordinates
+     */
+    public int getY() {
+        return y;
+    }
+}
---
+++ test/hk/edu/polyu/comp/comp2021/jungle/models/CoordinatesTest.java
@@ -0,0 +1,34 @@
+package hk.edu.polyu.comp.comp2021.jungle.models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+// TODO: Unit tests
+
+/**
+ * Unit test for Coordinates
+ */
+public class CoordinatesTest {
+
+    /**
+     * Test case for isValid
+     */
+    @Test
+    public void isValid() {
+    }
+
+    /**
+     * Test case for getX
+     */
+    @Test
+    public void getX() {
+    }
+
+    /**
+     * Test case for getY
+     */
+    @Test
+    public void getY() {
+    }
+}
