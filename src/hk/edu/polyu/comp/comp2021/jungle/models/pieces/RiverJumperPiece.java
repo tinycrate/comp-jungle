831f672c54bb03d572b6bacb20c4b65ad5f2c63f
SikyinTang <thomas2d19@gmail.com>
2018-11-20 19:13:37 +0800

    Completed River Jump

--- src/hk/edu/polyu/comp/comp2021/jungle/models/pieces/RiverJumperPiece.java
+++ src/hk/edu/polyu/comp/comp2021/jungle/models/pieces/RiverJumperPiece.java
@@ -5,7 +5,6 @@ import hk.edu.polyu.comp.comp2021.jungle.models.Coordinates;
 import hk.edu.polyu.comp.comp2021.jungle.models.Player;
 import hk.edu.polyu.comp.comp2021.jungle.models.tiles.Tile;
 import hk.edu.polyu.comp.comp2021.jungle.models.tiles.TileType;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 /**
  * This class contains default implementations for animals that can jump over rivers
@@ -25,12 +24,50 @@ public abstract class RiverJumperPiece extends Piece {
 
     @Override
     public boolean isMoveableTo(Coordinates coords, Board board) {
+
+        int moveDis = Math.abs(coords.getX() - getCoordinates().getX())
+                    + Math.abs(coords.getY() - getCoordinates().getY());
+
         // The animals are only allowed to move 1 tile in either direction, unless they are placed near the river
-        if (Math.abs(coords.getX() - getCoordinates().getX()) + Math.abs(coords.getY() - getCoordinates().getY()) != 1) {
+        if(moveDis < 1) return false;
+
+        if(moveDis > 1) {
             // TODO: 1) Check if the animal is currently near the river and if the destination is right at the other end of the river, if not return false
             // TODO: 2) Check if there's a rat intervening, if yes return false
             // TODO: 3) Remove the NotImplementedException and the todo comments when finished
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+
+
+            // Allow horizontal jump over river
+            if(coords.getX() == getCoordinates().getX()
+                    && (coords.getX() == 3 || coords.getX() == 4 || coords.getX() == 5)
+                    && Math.abs(coords.getY() - getCoordinates().getY()) == 3) {
+
+                // Check whether river tile has a rat, which is the only Piece that can go into river
+                int min = Math.min(getCoordinates().getY(), coords.getY());
+                Coordinates riverL = new Coordinates(coords.getX(), min + 1);
+                Coordinates riverR = new Coordinates(coords.getX(), min + 2);
+                if(board.getTile(riverL).isOccupied() || board.getTile(riverR).isOccupied()) {
+                    return false;
+                }
+                return true;
+
+            // Allow vertical jump over river
+            } else if(coords.getY() == getCoordinates().getY()
+                    && (coords.getY() == 1 || coords.getY() == 2 || coords.getY() == 4 || coords.getY() == 5)
+                    && Math.abs(coords.getX() - getCoordinates().getX()) == 4) {
+
+                // Check whether river tile has a rat, which is the only Piece that can go into river
+                int min = Math.min(getCoordinates().getX(), coords.getX());
+                Coordinates riverL = new Coordinates(coords.getY(), min + 1);
+                Coordinates riverM = new Coordinates(coords.getY(), min + 2);
+                Coordinates riverR = new Coordinates(coords.getY(), min + 3);
+                if(board.getTile(riverL).isOccupied() || board.getTile(riverM).isOccupied() || board.getTile(riverR).isOccupied()) {
+                    return false;
+                }
+                return true;
+            }
+            return false;
         }
 
         Tile tile = board.getTile(coords);
