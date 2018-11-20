36d63cb0bd5c28a96ed0e02f7a46c765eb403537
Ronald Chan <ronaldchanchl0@gmail.com>
2018-11-20 23:54:03 +0800

    Changed some text

--- src/hk/edu/polyu/comp/comp2021/jungle/views/ConsoleUIView.java
+++ src/hk/edu/polyu/comp/comp2021/jungle/views/ConsoleUIView.java
@@ -79,7 +79,7 @@ public class ConsoleUIView implements UIView {
 
     private void printWelcomeMessage() {
         // TODO: Beautify it
-        System.out.println("Welcome!!");
+        System.out.format("Welcome!! \nUse %s to start a new game \n Or %s to load a saved game.%n", CommandType.NEW.getCommandUsage(), CommandType.OPEN.getCommandUsage());
     }
 
     private void clearScreen() {
