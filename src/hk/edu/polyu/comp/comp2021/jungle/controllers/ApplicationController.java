15ba3865f3b45784b4a7fe3521619ecd94a70bde
Ronald Chan <12611939+tinycrate@users.noreply.github.com>
2018-11-21 01:48:36 +0800

    Game is now barely working. A lot of testing is needed.

--- src/hk/edu/polyu/comp/comp2021/jungle/controllers/ApplicationController.java
+++ src/hk/edu/polyu/comp/comp2021/jungle/controllers/ApplicationController.java
@@ -1,5 +1,7 @@
 package hk.edu.polyu.comp.comp2021.jungle.controllers;
 
+import hk.edu.polyu.comp.comp2021.jungle.views.ConsoleUIView;
+
 /**
  * This class handles the launching of the game
  */
@@ -7,7 +9,9 @@ public class ApplicationController {
     /**
      * Launches the game application
      */
-    public void Run(){
+    public void Run() {
         // starts the game
+        GameLogicController game = new GameLogicController(new ConsoleUIView());
+        game.Start();
     }
 }
