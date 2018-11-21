6643832d648aa8a7a1805e77185234f25a250b17
Ronald Chan <ronaldchanchl0@gmail.com>
2018-11-21 16:35:50 +0800

    Completely removed field coordinates from Piece All coordinates tracking is now handled by the board This fixed issues with inconsistency in coordinates update

--- src/hk/edu/polyu/comp/comp2021/jungle/models/pieces/BasicPiece.java
+++ src/hk/edu/polyu/comp/comp2021/jungle/models/pieces/BasicPiece.java
@@ -15,17 +15,16 @@ public abstract class BasicPiece extends Piece {
      * Creates a Piece object with reference to the board and its coordinates
      * Most likely called by board
      *
-     * @param coordinates The coordinares of the piece on the board
-     * @param owner       The owner of the piece
+     * @param owner The owner of the piece
      */
-    BasicPiece(Coordinates coordinates, Player owner) {
-        super(coordinates, owner);
+    BasicPiece(Player owner) {
+        super(owner);
     }
 
     @Override
     public boolean isMoveableTo(Coordinates coords, Board board) {
         // The animals are only allowed to move 1 tile in either direction
-        if (Math.abs(coords.getX() - getCoordinates().getX()) + Math.abs(coords.getY() - getCoordinates().getY()) != 1) {
+        if (Math.abs(coords.getX() - board.getCoordinates(this).getX()) + Math.abs(coords.getY() - board.getCoordinates(this).getY()) != 1) {
             return false;
         }
 
--- src/hk/edu/polyu/comp/comp2021/jungle/models/pieces/Cat.java
+++ src/hk/edu/polyu/comp/comp2021/jungle/models/pieces/Cat.java
@@ -1,6 +1,5 @@
 package hk.edu.polyu.comp.comp2021.jungle.models.pieces;
 
-import hk.edu.polyu.comp.comp2021.jungle.models.Coordinates;
 import hk.edu.polyu.comp.comp2021.jungle.models.Player;
 
 /**
@@ -12,11 +11,10 @@ public class Cat extends BasicPiece {
      * Creates a Piece object with reference to the board and its coordinates
      * Most likely called by board
      *
-     * @param coordinates The coordinares of the piece on the board
-     * @param owner       The owner of the piece
+     * @param owner The owner of the piece
      */
-    public Cat(Coordinates coordinates, Player owner) {
-        super(coordinates, owner);
+    public Cat(Player owner) {
+        super(owner);
     }
 
     @Override
