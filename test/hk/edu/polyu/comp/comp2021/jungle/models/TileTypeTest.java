94bbeeb6a22439d9f751763386845f27c4ab3b2e
Ken Ho <17056079d@connect.polyu.hk>
2018-11-21 15:57:27 +0800

    Added unit test

---
+++ test/hk/edu/polyu/comp/comp2021/jungle/models/TileTypeTest.java
@@ -0,0 +1,22 @@
+package hk.edu.polyu.comp.comp2021.jungle.models;
+import hk.edu.polyu.comp.comp2021.jungle.models.tiles.TileType;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ *
+ */
+public class TileTypeTest {
+    /**
+     *test case for vaild TileType
+     */
+    @Test
+    public void testDen() {
+        TileType test = TileType.DEN;
+        assertEquals("DEN",test.getName());
+        assertEquals("穴",test.getPlaceHolder());
+        assertFalse(test.isNeutralTile());
+
+    }
+}
