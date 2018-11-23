e64042009f8e5de6b62eec876bf57011190cd0ae
Ronald Chan <ronaldchanchl0@gmail.com>
2018-11-23 23:48:36 +0800

    Added hover effects, removed todo and fixed loading bugs

--- src/hk/edu/polyu/comp/comp2021/jungle/views/GameBoardPanel.java
+++ src/hk/edu/polyu/comp/comp2021/jungle/views/GameBoardPanel.java
@@ -14,17 +14,14 @@ import java.util.Map;
  * This class draws the game board
  */
 class GameBoardPanel extends JPanel {
-    /**
-     * The width of the panel
-     */
-    public static final int BOARD_WIDTH_PX = 463;
-    /**
-     * The height of the panel
-     */
-    public static final int BOARD_HEIGHT_PX = 598;
+
+    private static final int BOARD_WIDTH_PX = 463;
+    private static final int BOARD_HEIGHT_PX = 598;
 
     private static final int PIECE_WIDTH_PX = 64;
     private static final int PIECE_HEIGHT_PX = 64;
+
+    private static final float RECT_HOVER_TRANSPARENCY = 0.3f;
 
     private Board board;
     private final Timer updateTimer;
@@ -34,6 +31,8 @@ class GameBoardPanel extends JPanel {
 
     private final Image boardImage = ImageLoader.loadImageFromAssets("board.png");
 
+    private Rectangle hoveredRect;
+    private Piece selectedPiece;
 
     /**
      * Constructs the game screen
@@ -44,6 +43,20 @@ class GameBoardPanel extends JPanel {
         Dimension size = new Dimension(BOARD_WIDTH_PX, BOARD_HEIGHT_PX);
         setPreferredSize(size);
         setBackground(Color.WHITE);
+        addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                super.mouseMoved(e);
+                onMouseMoved(e);
+            }
+        });
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                onMouseClicked(e);
+            }
+        });
     }
 
     /**
@@ -77,10 +90,11 @@ class GameBoardPanel extends JPanel {
     /**
      * Checks if a board is being bound to this panel
      *
+     * @param board The board to be checked
      * @return True if it is already bound to a Board
      */
-    public boolean isBoardBound() {
-        return board != null;
+    public boolean isBoardBound(Board board) {
+        return board != null && this.board == board;
     }
 
     @Override
@@ -89,6 +103,15 @@ class GameBoardPanel extends JPanel {
         Graphics2D g2 = (Graphics2D) g;
         g2.drawImage(boardImage, 0, 0, null);
         if (board == null) return;
+        if (hoveredRect != null) {
+            Piece piece = board.getTile(hitboxMap.get(hoveredRect)).getOccupiedPiece();
+            if (piece != null) {
+                g2.setColor(Color.DARK_GRAY);
+                g2.setComposite(AlphaComposite.SrcOver.derive(RECT_HOVER_TRANSPARENCY));
+                g2.fillRect(hoveredRect.x, hoveredRect.y, hoveredRect.width, hoveredRect.height);
+                g2.setComposite(AlphaComposite.SrcOver);
+            }
+        }
         for (Piece piece : imageMap.keySet()) {
             Coordinates coords = board.getCoordinates(piece);
             if (coords != null) {
@@ -96,6 +119,19 @@ class GameBoardPanel extends JPanel {
                 g2.drawImage(imageMap.get(piece), point.x, point.y, null);
             }
         }
+    }
+
+    private void onMouseMoved(MouseEvent e) {
+        for (Rectangle rect : hitboxMap.keySet()) {
+            if (rect.contains(e.getPoint())) {
+                setHoveredRect(rect);
+                return;
+            }
+        }
+        setHoveredRect(null);
+    }
+
+    private void onMouseClicked(MouseEvent e) {
     }
 
     private void onUpdate(ActionEvent event) {
@@ -106,4 +142,7 @@ class GameBoardPanel extends JPanel {
         return new Point(coordinates.getX() * (PIECE_WIDTH_PX + 2) + 2, coordinates.getY() * (PIECE_HEIGHT_PX + 2) + 2);
     }
 
+    private void setHoveredRect(Rectangle hoveredRect) {
+        this.hoveredRect = hoveredRect;
+    }
 }
